10c156b25aac7a3565c7a293b47530ab6d66db66
Jamie Nelson <jfn4th@gmail.com>
2020-04-29 22:51:28 -0500

    add scoring descriptions

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import './RuleRow.css'
+import './RuleRow.css';
 
 class RuleRow extends Component {
-  render() {
-    return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
-      </tr>
-    )
-  }
+    render() {
+        const active = this.props.score === undefined;
+        const classes = `RuleRow ${active ? 'RuleRow-active' : 'RuleRow-disabled'}`;
+        return (
+            <tr className={classes} onClick={active ? this.props.doScore : ''}>
+                <td className='RuleRow-name'>{this.props.name}</td>
+                <td className='RuleRow-score'>{active ? this.props.description : this.props.score}</td>
+            </tr>
+        );
+    }
 }
 
 export default RuleRow;
