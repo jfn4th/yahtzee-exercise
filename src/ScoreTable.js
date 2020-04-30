b4f5656cfe541e693595eee9caefdd26e49cb120
Jamie Nelson <jfn4th@gmail.com>
2020-04-30 17:39:43 -0500

    add total score to score table

--- src/ScoreTable.js
+++ src/ScoreTable.js
@@ -18,6 +18,14 @@ import {
 } from './Rules';
 
 class ScoreTable extends Component {
+    getTotalScore() {
+        const { scores } = this.props;
+        let totalScore = 0;
+        for (const score in scores) {
+            if (scores[score]) totalScore += scores[score];
+        }
+        return totalScore;
+    }
     render() {
         const { scores, doScore } = this.props;
 
@@ -115,6 +123,7 @@ class ScoreTable extends Component {
                         </tbody>
                     </table>
                 </section>
+                <h2>TOTAL SCORE: {this.getTotalScore()}</h2>
             </div>
         );
     }
