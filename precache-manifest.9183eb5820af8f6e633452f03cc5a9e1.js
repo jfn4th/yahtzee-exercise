94c12250ba6f894b24947e5b8c99efbf8e0ed7b6
Jamie Nelson <jfn4th@gmail.com>
2020-05-19 23:50:22 -0500

    Updates

---
+++ precache-manifest.9183eb5820af8f6e633452f03cc5a9e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee59fe49244443a601d2f35b0816620",
+    "url": "/yahtzee-exercise/index.html"
+  },
+  {
+    "revision": "ab9c2ef12f496f386705",
+    "url": "/yahtzee-exercise/static/css/main.53d5d3e9.chunk.css"
+  },
+  {
+    "revision": "4163823aa3652622a001",
+    "url": "/yahtzee-exercise/static/js/2.f4fcb76c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yahtzee-exercise/static/js/2.f4fcb76c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9c2ef12f496f386705",
+    "url": "/yahtzee-exercise/static/js/main.50a9c0dd.chunk.js"
+  },
+  {
+    "revision": "98e6ce75df5128c17782",
+    "url": "/yahtzee-exercise/static/js/runtime-main.9f394ba7.js"
+  }
+]);
